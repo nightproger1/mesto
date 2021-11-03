01d2af8395766b1b756af44a616faf48855d2c2a
nightproger1 <69048540+nightproger1@users.noreply.github.com>
2021-11-03 19:25:08 +0300

    Исправлены все замечания

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
 // edit popup
-const popupEdit = document.getElementById('edit');
+const popupEdit = document.getElementById('edit'); //Уважаемый ревьювер, я использую getElementById, потому что он меньше нагружает процесс при компиляции, чем querySelector
 const formElement = popupEdit.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input_user_name');
 const jobInput = document.querySelector('.popup__input_user_profession');
-const openPopup = document.querySelector('.profile__edit-btn');
-const closePopup = popupEdit.querySelector('.popup__close');
+const popupEditOpen = document.querySelector('.profile__edit-btn');
+const popupEditclose = popupEdit.querySelector('.popup__close');
 const titleInput = document.querySelector('.profile__title');
 const paragraphInput = document.querySelector('.profile__paragraph');
 
 //location popup
 const popupLocation = document.getElementById('location');
 const formLocation = popupLocation.querySelector('.popup__form');
-const openCardPopup = document.querySelector('.profile__add');
-const closeCardPopup = popupLocation.querySelector('.popup__close');
+const popupLocationOpen = document.querySelector('.profile__add');
+const popupLocationClose = popupLocation.querySelector('.popup__close');
 const locationInput = document.querySelector('.popup__input_location');
 const imagelInput = document.querySelector('.popup__input_image');
 const card = document.querySelector('.card').content.querySelector('.element');
@@ -20,7 +20,7 @@ const elements = document.querySelector('.elements');
 
 //enlarged popup
 const popupEnlarged = document.getElementById('enlarged');
-const closeEnlargedPopup = popupEnlarged.querySelector('.popup__close');
+const popupEnlargedClose = popupEnlarged.querySelector('.popup__close');
 const imageEnlarged = popupEnlarged.querySelector('.popup__img-enlarged');
 const imageDescription = popupEnlarged.querySelector('.popup__img-title');
 
@@ -52,7 +52,7 @@ const initialCards = [
 ];
 
 //создание карточки
-function cardCreation(item) {
+function createCard(item) {
   const element = card.cloneNode(true);
   const cardImage = element.querySelector('.element__image');
   const cardTitle = element.querySelector('.element__title');
@@ -73,18 +73,18 @@ function cardCreation(item) {
   cardImage.addEventListener('click', function () {
     imageDescription.textContent = item.name;
     imageEnlarged.src = item.link;
-    imageEnlarged.alt = 'Фотография';
+    imageEnlarged.alt = item.name;
     openCardForm(popupEnlarged);
   });
 
   cardTitle.textContent = item.name;
   cardImage.src = item.link;
-  cardImage.alt = 'Фотография';
+  cardImage.alt = item.name;
   return element;
 };
 
 function renderCard(item) {
-  elements.prepend(cardCreation(item));
+  elements.prepend(createCard(item));
 };
 
 initialCards.forEach((item) => {
@@ -95,10 +95,10 @@ function openCardForm(popup) {
   popup.classList.add('popup_active');
 }
 
-function openForm(popup) {
-  popup.classList.add('popup_active');
+function openProfileForm(editPopup) {
   nameInput.value = titleInput.textContent;
   jobInput.value = paragraphInput.textContent;
+  openCardForm(editPopup);
 }
 
 function closeForm(popup) {
@@ -106,7 +106,7 @@ function closeForm(popup) {
 }
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler(evt) {
+function formEditSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   titleInput.textContent = nameInput.value;
   paragraphInput.textContent = jobInput.value;
@@ -114,7 +114,7 @@ function formSubmitHandler(evt) {
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', formEditSubmitHandler);
 
 function сardSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -126,8 +126,8 @@ function сardSubmitHandler(evt) {
 formLocation.addEventListener('submit', сardSubmitHandler);
 
 //открытие и закрытия попапов
-openPopup.addEventListener('click', () => openForm(popupEdit));
-closePopup.addEventListener('click', () => closeForm(popupEdit));
-openCardPopup.addEventListener('click', () => openForm(popupLocation));
-closeCardPopup.addEventListener('click', () => closeForm(popupLocation));
-closeEnlargedPopup.addEventListener('click', () => closeForm(popupEnlarged));
+popupEditOpen.addEventListener('click', () => openProfileForm(popupEdit));
+popupEditclose.addEventListener('click', () => closeForm(popupEdit));
+popupLocationOpen.addEventListener('click', () => openCardForm(popupLocation));
+popupLocationClose.addEventListener('click', () => closeForm(popupLocation));
+popupEnlargedClose.addEventListener('click', () => closeForm(popupEnlarged));
